79acc6a8666eeaa605908389201cc831021ad15d
Kate Beaumont <89060764+katecoderacademy@users.noreply.github.com>
2022-02-13 13:13:55 +1100

    Revert "added final styling for merge"

--- src/components/AccountPage.js
+++ src/components/AccountPage.js
@@ -1,34 +1,25 @@
 import React from 'react'
 
+
 export function AccountPage(props) {
     return (
-        <>
-            <heading>My Account</heading>
-        
-        
-            <div className='edit-my-account' ><a href='/NewPost'>
-                <div><button >New Post</button></div>
-            </a> </div>
+        <div>
+            <div className='MyPosts'><a href='/NewPost'>
+                <button>New Post</button>
+            </a>
                 {/* Api fetch call to get this users posts */}
 
-           
-            {/* <div className='MyDetails'> */}
+            </div>
+            <div className='MyDetails'>
                 {/* Api fetch to get username, email, password with a check to see if dark mode is active? */}
-                
-                
-                <div className='my-account'>
-                <label>Username:</label><button>edit</button>
-                <label>Email:</label><button>edit</button>
-                <label>Password: ********</label>
-                <button>reveal</button>
-                </div>
-                <div className='edit-my-account'>
-                <button>edit</button>
-                <p /><button>delete account</button></div>
-                {/* <p><label>Darkmode</label></p> */}
+                <p>Username:</p><button>edit</button>
+                <p>Email:</p><button>edit</button>
+                <p>Password: ********</p><button>reveal</button><button>edit</button>
+                <button>delete account</button>
+                <p>Darkmode</p>
                 {/* value={props.darkMode} ? */}
-                {/* <input type="checkbox" ></input> */}
-            {/* </div> */}
-        </>
+                <input type="checkbox" ></input>
+            </div>
+        </div>
     )
 }
--- src/components/JobPosts.js
+++ src/components/JobPosts.js
@@ -8,15 +8,9 @@ export function JobPosts() {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(1);
   useEffect(() => {
-    axios.get("http://localhost:3000/api/posts").then((res) => setPosts(res.data));
-    axios.get("http://localhost:3000/api/categories").then((res) => setCategories(res.data));
-
-
-
-    // setSelectedCategory(localStorage.getItem('selectedCategory'));
-    // selectedCategory.addEventListener('change', function(){
-    //   footer.style.setProperty('--category-image', setCategories)
-    // })
+    axios.get("http://whatjobrailsapi.herokuapp.com/api/posts").then((res) => setPosts(res.data));
+    axios.get("http://whatjobrailsapi.herokuapp.com/api/categories").then((res) => setCategories(res.data));
+    setSelectedCategory(localStorage.getItem('selectedCategory'));
   }, []);
 
   const handleIncrementLikes = () => {
@@ -34,68 +28,66 @@ export function JobPosts() {
     console.log(localStorage.getItem('selectedCategory'));
   }
 
-
-  
   return (
     <>
-      <heading>Job Posts</heading>
-      
       <div>
-        <form ><div class="category-select">
+        <form >
           <label >Choose a field:</label>
-
           <select onChange={handleCategory} value={selectedCategory} id="Jobs" name="Jobs" size="1">
             {categories.map((category) => (
               <option key={category.id} value={category.id}>{category.name}</option>
             ))
             }
-          </select></div>
+          </select>
         </form>
-        
-        
-        
-        <div className="jobs-by-category"><h1>Category Name</h1>
+        <h1>Category Name</h1>
         <h2>Role Title</h2>
         <div>
-          
+          <div>
             {
               posts.map((post) => (
                 <div key={post.id}>
                   {(() => {
                     if (post.category_id == selectedCategory) {
                       return (
-                        <border className='jobs-by-category-border'>
-                        <post className='jobs-by-category'>
-                          <p><label>User:</label> {post.user_id}</p>
-                          <p><label>Title:</label> {post.title}</p>
-                          <p><label>Introduction:</label> {post.intro}</p>
-                          <p><label>Day to day:</label> {post.day_to_day}</p>
-                          <p><label>Job difficulty: {post.difficulty}/10</label></p>
-                          <p><label>
-                            Expected salary range</label> from: {post.expected_salary_range_from} to: {post.expected_salary_range_to}</p>
-                            <p><label>Industry growth:</label> {post.industry_growth}</p>
-                            <p><label>
-                            Work life balance:</label> {post.work_life_balance}</p>
-                            <p><label>Career Path:</label> {post.career_path}</p>
-                            <p><label>
-                            good post:</label> {post.upvotes}</p>
-                            
-                            <p><label>
-                            bad post:</label> {post.downvotes}</p>
-                          
-                            <button><a href="/">Link to Deep Dive</a></button>
+                        <div>
+                          <div>
+                            User: {post.user_id}
+                          </div><div>
+                            Title: {post.title}
+                          </div><div>
+                            Introduction: {post.intro}
+                          </div><div>
+                            Day to day: {post.day_to_day}
+                          </div><div>
+                            Job difficulty: {post.difficulty}/10
+                          </div><div>
+                            Expected salary range from: {post.expected_salary_range_from} to: {post.expected_salary_range_to}
+                          </div><div>
+                            Industry growth: {post.industry_growth}
+                          </div><div>
+                            Work life balance: {post.work_life_balance}
+                          </div><div>
+                          </div><div>
+                            Career Path: {post.career_path}
+                          </div><div>
+                            good post: {post.upvotes}
+                          </div><div>
+                            bad post: {post.downvotes}
+                          </div>
+                          <button><a href="/">Link to Deep Dive</a></button>
                           <div> _</div>
-                        </post></border>)
+                        </div>)
                     }
                   })()}
                 </div>
 
               ))
             }
-          
+          </div>
         </div>
-      
-      <div className="jobs-by-category">
+      </div>
+      <div>
         <button onClick={handleIncrementLikes}>(Bootstrap thumbs up)</button>
         <h5>Count is {likes}</h5>
         <button onClick={handleIncrementDisLikes}>(Bootstrap thumbs up)</button>
@@ -104,8 +96,6 @@ export function JobPosts() {
       <button onClick={() => setLikes(0)}></button>
       <div></div>
       <button onClick={() => setDisLikes(0)}></button>
-      </div>
-      </div>
     </>
   );
 }
